9deb19ce0f57b4e271f326b3fc3e379367d6d472
iamgodot <xugodot@gmail.com>
2021-09-30 17:23:12 +0800

    feat: package code2077.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='code2077',
+    version='0.1.0',
+    author='Godot',
+    author_email='xugodot@gmail.com',
+    packages=find_packages(exclude=('tests', )),
+    url='https://github.com/iamgodot/code2077',
+    license='MIT',
+    description='Code for ds and alg.',
+    install_requires=[],
+)
