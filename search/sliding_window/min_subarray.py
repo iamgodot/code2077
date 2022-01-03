03d922e2014e8e6596c78ebccea98a0fd8ae5eb9
iamgodot <xugodot@gmail.com>
2022-01-03 23:55:27 +0800

    Add sliding_window for array

--- search/sliding_window/min_subarray.py
+++ search/sliding_window/min_subarray.py
@@ -3,26 +3,25 @@
 
 
 # 1. Brute force 双层 for 循环，时间复杂度 O(n^2)
-# 2. 外层 for 循环加二分查找，时间复杂度 O(n*logn)
-# 3. 头部双指针，构造滑动窗口，时间复杂度 O(n)
+# 2. 因为数组中都是正数，所以在外层 for 循环中可以使用前缀和进行二分查找，时间复杂度 O(n*logn)
+# 3. 同样因为数组中都是正数，所以可以应用滑动窗口，时间复杂度 O(n)
 def min_subarray_len(target: int, nums: list) -> int:
-    res = 0
-    left, right = 0, 0
+    length = len(nums)
+    res = length + 1
+    total = left = right = 0
 
-    while left < len(nums) and right < len(nums):
-        sum_ = sum(nums[left:right + 1])
-
-        if sum_ < target:
-            right += 1
-        else:
-            length = right - left + 1
-            res = min(res, length) if res != 0 else length
+    while right < length:
+        total += nums[right]
+        while total >= target:
+            res = min(res, right - left + 1)
+            total -= nums[left]
             left += 1
+        right += 1
 
-    return res
+    return 0 if res == length + 1 else res
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert min_subarray_len(1, []) == 0
     assert min_subarray_len(7, [2, 3, 1, 2, 4, 3]) == 2
     assert min_subarray_len(4, [1, 4, 4]) == 1
