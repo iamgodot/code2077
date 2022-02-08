1c335df86d35eef2f0db39da90ec3abd455fa84f
iamgodot <xugodot@gmail.com>
2022-02-08 08:42:28 +0800

    Update missing_number for bitmap

--- basic/bitmap/missing_number.py
+++ basic/bitmap/missing_number.py
@@ -1,22 +1,10 @@
-# 在一个无序数组里有 99 个不重复的正整数，范围是 1~100，唯独缺少一个 1~100 中的整数
-# 如何找出这个整数
+# 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
 
-# 可以先求出 1~100 的和，然后减去数组中的所有的数字，结果就是要找的整数了
+# 1. 可以通过数学来求 nums 的和，然后再用 0-n 的和减去它
+# 2. 也可以把 n 个数再加上 0-n 的 n+1 个数，再对这 2n+1 个数做异或
 def find_missing_number(nums: list) -> int:
-    return sum(range(1, 101)) - sum(nums)
-
-
-# 一个无序数组里面有若干个正整数，范围是 1~100，其中 99 个整数都出现了偶数次
-# 只有一个整数出现了奇数次，如何找出这个整数
-
-# 两个相同的数字的异或结果为 0，一个数字和 0 的异或结果为这个数字本身
-# 所以直接把所有的元素都做一遍异或就可以得到落单的整数了
-def find_missing_number2(nums: list) -> int:
-    res = 0
-    for i in nums:
-        res ^= i
-
-    return res
+    n = len(nums)
+    return n * (n + 1) // 2 - sum(nums)
 
 
 # 如果有两个整数出现了奇数次，如何找出这两个整数
@@ -24,7 +12,7 @@ def find_missing_number2(nums: list) -> int:
 # 所有整数的异或结果相当于这两个整数的异或结果，根据此结果可以判断两个数是从二进制的
 # 第几位开始不同的，通过这一位可以将所有的数分成两部分，每一部分各包含一个落单的整数
 # 再分别异或即可
-def find_missing_number3(nums: list) -> list:
+def find_missing_number2(nums: list) -> list:
     res = [0, 0]
     xor_res = 0
     for i in nums:
@@ -33,17 +21,28 @@ def find_missing_number3(nums: list) -> list:
     while xor_res & separator == 0:
         separator <<= 1
     for i in nums:
-        if i & separator == 0:
+        if i & separator == 0:  # 注意这里的两种结果是 0/separator 而不是 0/1
             res[0] ^= i
         else:
             res[1] ^= i
     return res
 
 
+# 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+
+
+# 思路是通过累加每一位二进制位的数字并做 3 的余数，结果就是只出现一次的数字
+# 使用状态机可以实现，利用两位二进制数表达 0/1/2 三种余数
+# 时间复杂度 O(n) 空间复杂度 O(1)
+def single_number(nums: list) -> int:
+    ones = twos = 0
+    for num in nums:
+        ones = ones ^ num & ~twos
+        twos = twos ^ num & ~ones
+    return ones
+
+
 if __name__ == "__main__":
-    nums = [i for i in range(1, 50)] + [i for i in range(51, 101)]
-    assert find_missing_number(nums) == 50
-    nums = [i for i in range(1, 100)] * 2 + [101]
-    assert find_missing_number2(nums) == 101
+    assert find_missing_number([9, 6, 4, 2, 3, 5, 7, 0, 1]) == 8
     nums = [i for i in range(1, 100)] * 2 + [101, 102]
-    assert sorted(find_missing_number3(nums)) == [101, 102]
+    assert sorted(find_missing_number2(nums)) == [101, 102]
