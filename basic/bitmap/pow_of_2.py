566cc3eda48609336540984ae4a7f7d71f9835a9
iamgodot <xugodot@gmail.com>
2022-02-08 08:42:49 +0800

    Update pow_of_2 for bitmap

--- basic/bitmap/pow_of_2.py
+++ basic/bitmap/pow_of_2.py
@@ -5,9 +5,20 @@
 # 要注意 0 不是 2 的整数次幂，但是 0 和任何数做和运算都得到 0
 # 如果是负数的话也要排除
 def pow_of_2(n: int) -> bool:
-    if n <= 0:
-        return False
-    return n & n - 1 == 0
+    return n > 0 and n & n - 1 == 0
+
+
+# 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
+
+# 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
+
+
+# 如果满足条件那么除了是 2 的整数次幂另外二进制的 1/3/5 等位置上肯定都是 1
+# 时间复杂度 O(1) 空间复杂度 O(1)
+def pow_of_4(n: int) -> bool:
+    return (
+        n > 0 and n & n - 1 == 0 and n & int("10101010101010101010101010101010", 2) == 0
+    )
 
 
 if __name__ == "__main__":
@@ -16,3 +27,6 @@ if __name__ == "__main__":
     assert pow_of_2(1) is True
     assert pow_of_2(8) is True
     assert pow_of_2(10) is False
+    assert pow_of_4(16) is True
+    assert pow_of_4(5) is False
+    assert pow_of_4(8) is False
