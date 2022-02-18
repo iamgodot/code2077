45cb9be419540f7b97f48c0d39cca037272d2505
iamgodot <xugodot@gmail.com>
2022-02-19 00:02:07 +0800

    Update min_subarray with prefix sum solution

--- search/sliding_window/min_subarray.py
+++ search/sliding_window/min_subarray.py
@@ -3,7 +3,47 @@
 
 
 # 1. Brute force 双层 for 循环，时间复杂度 O(n^2)
-# 2. 因为数组中都是正数，所以在外层 for 循环中可以使用前缀和进行二分查找，时间复杂度 O(n*logn)
+# 2. 因为数组中都是正数，所以在外层 for 循环中可以使用前缀和进行二分查找
+# 时间复杂度 O(n*logn), 空间复杂度 O(n)
+def min_subarray_len(target: int, nums: list) -> int:
+    """
+    1. Build prefix sum array: ps[i] = sum(nums[0] + .. + nums[i])
+    2. Iterate nums, for every num, find the smallest j and let: ps[j] - ps[i] + nums[i] >= target
+    3. Update res with minimum value
+    """
+
+    def bs(nums, target, left, right):
+        if left > right:
+            return -1
+        while left <= right:
+            mid = (left + right) // 2
+            if target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+
+    res = float("inf")
+    prefix_sums = []
+    total = 0
+    for num in nums:
+        total += num
+        prefix_sums.append(total)
+
+    length = len(nums)
+    for i in range(length):
+        index = bs(prefix_sums, target + prefix_sums[i] - nums[i], i, length - 1)
+        if index == 0:
+            res = min(res, i + 1)
+        elif index > length - 1:
+            pass
+        else:
+            res = min(res, index - i + 1)
+
+    return 0 if res == float("inf") else res
+
+
 # 3. 同样因为数组中都是正数，所以可以应用滑动窗口，时间复杂度 O(n)
 def min_subarray_len(target: int, nums: list) -> int:
     length = len(nums)
