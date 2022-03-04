d43587ed4b4241357c38e2ac9dfb122f9d889fce
iamgodot <xugodot@gmail.com>
2022-03-04 21:10:20 +0800

    Refine code for cycle of linked list

--- data_structures/linked_list/cycle.py
+++ data_structures/linked_list/cycle.py
@@ -1,16 +1,14 @@
 # 给定一个链表，判断链表中是否有环。
 # 如果链表中存在环，则返回 true 。 否则，返回 false 。
 
-from linked_list import ListNode
+from data_structures.linked_list import ListNode
 
 
 def has_cycle(head: ListNode) -> bool:
     slow = fast = head
 
     while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
-
+        slow, fast = slow.next, fast.next.next
         if slow is fast:
             return True
 
@@ -21,8 +19,7 @@ def detect_cycle(head: ListNode) -> ListNode:
     slow = fast = head
 
     while fast and fast.next:
-        slow = slow.next
-        fast = fast.next.next
+        slow, fast = slow.next, fast.next.next
 
         if slow is fast:
             m, n = head, slow
