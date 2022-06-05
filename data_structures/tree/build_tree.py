51daada1f03da872ada2af1b7fbc083c13adb683
iamgodot <xugodot@gmail.com>
2022-06-05 11:35:26 +0800

    Refine build_tree

--- data_structures/tree/build_tree.py
+++ data_structures/tree/build_tree.py
@@ -7,20 +7,19 @@ from data_structures.tree import TreeNode
 
 # 1. preorder 确认根节点和左子节点 -> 2. inorder 确认根节点左右的子树长度 -> 3. preorder 确认两个子树的根节点位置，其实主要是右子节点的位置
 # 时间复杂度 O(n)
-# 空间复杂度 O(n) 其中哈希表占用 O(n) 递归占用 O(h)
+# 空间复杂度 O(h)
 def build_tree(preorder: List[int], inorder: List[int]) -> TreeNode:
     def _build(index, left, right) -> TreeNode:
         if left > right:
             return None
         val = preorder[index]
         root = TreeNode(val)
-        i = hashtable[val]
+        i = inorder.index(val)
         root.left = _build(index + 1, left, i - 1)
         # 左子节点的位置 index+1 加上左子树的长度 i-1-left+1
         root.right = _build(index + 1 + i - left, i + 1, right)
         return root
 
-    hashtable = {val: pos for pos, val in enumerate(inorder)}
     return _build(0, 0, len(inorder) - 1)
 
 
@@ -35,28 +34,9 @@ def build_tree(inorder: List[int], postorder: List[int]) -> TreeNode:
             return None
         val = postorder[index]
         root = TreeNode(val)
-        i = hashtable[val]
+        i = inorder.index(val)
         root.left = build(index - 1 - (right - i), left, i - 1)
         root.right = build(index - 1, i + 1, right)
         return root
 
-    hashtable = {val: pos for pos, val in enumerate(inorder)}
     return build(len(postorder) - 1, 0, len(inorder) - 1)
-
-
-# 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
-# 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
-
-
-def sorted_array_to_bst(nums: List[int]) -> TreeNode:
-    def build(left, right) -> TreeNode:
-        if left > right:
-            return None
-
-        mid = (left + right) // 2
-        node = TreeNode(nums[mid])
-        node.left = build(left, mid - 1)
-        node.right = build(mid + 1, right)
-        return node
-
-    return build(0, len(nums) - 1)
