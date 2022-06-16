d54f4523b7df728cd8fe7f196359988ba820f4da
iamgodot <xugodot@gmail.com>
2022-06-16 10:30:35 +0800

    Add kth_element for tree

--- data_structures/tree/kth_element.py
+++ data_structures/tree/kth_element.py
@@ -6,7 +6,7 @@ from data_structures.tree import TreeNode
 # 时间复杂度 O(n) 当树退化为链表时（全部为右子节点），无论 k 的值大小，递归深度都为 N ，占用 O(n) 时间
 # 空间复杂度 O(n) 当树退化为链表时（全部为右子节点），系统使用 O(n) 大小的栈空间
 def kth_largest(root: TreeNode, k: int) -> int:
-    def traversal(root):
+    def traversal(root) -> None:
         nonlocal res, k
         if not root:
             return
@@ -22,7 +22,7 @@ def kth_largest(root: TreeNode, k: int) -> int:
     return res
 
 
-def kth_largest(root, k) -> int:
+def kth_largest_it(root, k) -> int:
     cur, stack = root, []
     while cur or stack:
         if cur:
@@ -36,6 +36,27 @@ def kth_largest(root, k) -> int:
             cur = cur.left
 
 
+# 给定一棵二叉搜索树，请找出其中第 k 大的节点的值。
+
+
+def kth_smallest(root: TreeNode, k: int) -> int:
+    def traversal(node: TreeNode) -> None:
+        if not node:
+            return
+        traversal(node.left)
+        nonlocal k, res
+        k -= 1
+        if k == 0:
+            res = node.val
+            return
+        traversal(node.right)
+
+    res = None
+    traversal(root)
+    return res
+
+
 if __name__ == "__main__":
-    root = TreeNode(3, left=TreeNode(1, right=TreeNode(2)), right=TreeNode(4))
-    assert kth_largest(root, 1) == 4
+    tree = TreeNode(3, left=TreeNode(1, right=TreeNode(2)), right=TreeNode(4))
+    assert kth_largest(tree, 2) == 3
+    assert kth_smallest(tree, 2) == 2
