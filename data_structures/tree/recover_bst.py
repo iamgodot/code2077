b276d099ac534d21f6d2dbc32a4bacbd26c4cda9
iamgodot <xugodot@gmail.com>
2022-06-26 12:29:20 +0800

    Add docstring for Morris traversal

--- data_structures/tree/recover_bst.py
+++ data_structures/tree/recover_bst.py
@@ -31,7 +31,16 @@ def recover_tree(root: Optional[TreeNode]) -> None:
     x.val, y.val = y.val, x.val
 
 
-def recover_tree(root: Optional[TreeNode]) -> None:
+def recover_tree2(root: Optional[TreeNode]) -> None:
+    """
+    使用莫里斯遍历达到 O(1) 的空间复杂度。
+    1. 如果存在左子节点：寻找左子树的最右节点，核心是为了增加链接以后跳回来。
+        1.1. 如果链接不存在，增加链接，然后跳到左子节点。
+        1.2. 如果链接存在，说明左子树已经遍历完毕，对当前节点执行逻辑，断开链接，跳到右子节点。
+    2. 如果不存在左子节点：对当前节点执行逻辑，跳到右子节点。
+
+    可以看出来 1.2 和 2 做的事情基本是重复的，除了断开链接的部分。
+    """
     pre = None
     x = y = None
     while root:
@@ -61,9 +70,10 @@ def recover_tree(root: Optional[TreeNode]) -> None:
 
 
 if __name__ == "__main__":
-    root = TreeNode(1, left=TreeNode(3, right=TreeNode(2)))
-    recover_tree(root)
-    assert levelorder(root) == [[3], [1], [2]]
-    root = TreeNode(3, left=TreeNode(1), right=TreeNode(4, left=TreeNode(2)))
-    recover_tree(root)
-    assert levelorder(root) == [[2], [1, 4], [3]]
+    for func in [recover_tree, recover_tree2]:
+        tree = TreeNode(1, left=TreeNode(3, right=TreeNode(2)))
+        func(tree)
+        assert levelorder(tree) == [[3], [1], [2]]
+        tree = TreeNode(3, left=TreeNode(1), right=TreeNode(4, left=TreeNode(2)))
+        func(tree)
+        assert levelorder(tree) == [[2], [1, 4], [3]]
