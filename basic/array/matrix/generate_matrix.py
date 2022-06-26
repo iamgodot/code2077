45c29bc3a5ebc44698c1807645568ee3c0baafe9
iamgodot <xugodot@gmail.com>
2022-06-26 12:40:58 +0800

    Fix lint

--- basic/array/matrix/generate_matrix.py
+++ basic/array/matrix/generate_matrix.py
@@ -35,9 +35,13 @@ def generate_matrix(n: int) -> list:
     return matrix
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert generate_matrix(0) == []
     assert generate_matrix(1) == [[1]]
     assert generate_matrix(3) == [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
-    assert generate_matrix(4) == [[1, 2, 3, 4], [12, 13, 14, 5],
-                                  [11, 16, 15, 6], [10, 9, 8, 7]]  # noqa
+    assert generate_matrix(4) == [
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7],
+    ]  # noqa
