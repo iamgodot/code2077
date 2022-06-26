289d47bc408e7972b7cbdf39f1a7cec56c7ad534
iamgodot <xugodot@gmail.com>
2022-06-26 13:56:40 +0800

    Remove duplicate

--- data_structures/stack/valid_parentheses.py
+++ data_structures/stack/valid_parentheses.py
@@ -1,24 +1,30 @@
 # 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
 
 
-# 时间和空间复杂度都是 O(n)
 def is_valid(s: str) -> bool:
-    SYMBOL_MAPPINGS = {')': '(', '}': '{', ']': '['}
-    stack = list()
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    mappings = {")": "(", "}": "{", "]": "["}
+    stack = []
 
     for char in s:
-        if char in '({[':
+        if char in "({[":
             stack.append(char)
         else:
-            if not stack or stack.pop() != SYMBOL_MAPPINGS[char]:
+            if not stack or stack.pop() != mappings[char]:
                 return False
 
     return len(stack) == 0
 
 
-if __name__ == '__main__':
-    for s, validity in ('()', True), ('(){}[]',
-                                      True), ('(]', False), ('([)]',
-                                                             False), ('{[]}',
-                                                                      True):
-        assert is_valid(s) == validity
+if __name__ == "__main__":
+    for string, validity in (
+        ("()", True),
+        ("(){}[]", True),
+        ("(]", False),
+        ("([)]", False),
+        ("{[]}", True),
+    ):
+        assert is_valid(string) == validity
