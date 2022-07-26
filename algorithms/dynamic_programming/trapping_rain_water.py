2c6a5296d9a56536912f1c41fe515dd1466c500b
iamgodot <xugodot@gmail.com>
2022-07-26 23:07:32 +0800

    Remove mono stack solution and refine code

--- algorithms/dynamic_programming/trapping_rain_water.py
+++ algorithms/dynamic_programming/trapping_rain_water.py
@@ -6,31 +6,6 @@ from typing import List
 
 def trap(height: List[int]) -> int:
     """
-    单调栈解法：
-    1. 栈中保存下标。
-    2. 遍历元素，遇到更大元素并且栈中存在两个元素及以上才可能增加 res。
-    3. 计算增量时注意高度乘以宽度，因为宽度不一定总是为一的。
-
-    Time: O(n)
-    Space: O(n)
-    """
-    stack = []
-    res = 0
-    for i in range(len(height)):
-        while stack and height[i] > height[stack[-1]]:
-            # 无论 stack 是否有两个元素都需要 pop 出来
-            # 保证 height gap 计算的准确
-            top = stack.pop()
-            if not stack:
-                break
-            width = i - stack[-1] - 1
-            res += (min(height[stack[-1]], height[i]) - height[top]) * width
-        stack.append(i)
-    return res
-
-
-def trap2(height: List[int]) -> int:
-    """
     动态规划：
     1. 对于每个元素来说，雨水增量等于左右最大高度的最小值减去当前高度。
     2. 如果在每个位置都重新扫描左右两边的话总体复杂度为 O(n^2)。
@@ -57,34 +32,37 @@ def trap2(height: List[int]) -> int:
     return res
 
 
-def trap3(height: List[int]) -> int:
+def trap2(height: List[int]) -> int:
     """
     双指针：
     1. 在扫描过程中，用两个变量维护每个位置的左右最大值集合。
-    2. 如果 height[i] 小于 height[j]，那么左右两边最大值的较小者一定是 max_left
-        而 max_left 对于 i 指针来说是确定的，因为是从左向右扫描。
+    2. 如果 max_left 小于 max_right，那么左右两边最大值的较小者一定是 max_left
+        所以左指针右移，然后比较 max_left 与 height[i] 决定 res 的增量。
     3. 反之则可以确定 max_right 的值。
+
+    注意对于 height 为空的情况要单独判断。
 
     Time: O(n)
     Space: O(1)
     """
+    if not height:
+        return 0
     res = 0
     i, j = 0, len(height) - 1
-    max_left = max_right = 0
+    max_left, max_right = height[i], height[j]
 
     while i < j:
-        max_left = max(max_left, height[i])
-        max_right = max(max_right, height[j])
-        if height[i] < height[j]:
-            res += max_left - height[i]
+        if max_left < max_right:
             i += 1
+            max_left = max(max_left, height[i])
+            res += max_left - height[i]
         else:
-            res += max_right - height[j]
             j -= 1
-
+            max_right = max(max_right, height[j])
+            res += max_right - height[j]
     return res
 
 
 if __name__ == "__main__":
-    for func in trap, trap2, trap3:
+    for func in trap, trap2:
         assert func([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
