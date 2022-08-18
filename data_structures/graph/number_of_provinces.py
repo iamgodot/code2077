9d3300be2a910e1a8419a40cffc2179813e380fb
iamgodot <xugodot@gmail.com>
2022-08-18 11:40:50 +0800

    Add number_of_provinces for graph

---
+++ data_structures/graph/number_of_provinces.py
@@ -0,0 +1,35 @@
+# 有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
+# 省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
+# 给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
+# 返回矩阵中 省份 的数量。
+
+from typing import List
+
+
+def find_circle_num(isConnected: List[List[int]]) -> int:
+    """
+    Time: O(n^2)
+    Space: O(n)
+    """
+    n = len(isConnected)
+    from collections import deque
+
+    visited = set()
+    res = 0
+    for city in range(n):
+        if city not in visited:
+            dq = deque([city])
+            visited.add(city)
+            while dq:
+                i = dq.pop()
+                for j in range(n):
+                    if isConnected[i][j] == 1 and j not in visited:
+                        dq.appendleft(j)
+                        visited.add(j)
+            res += 1
+    return res
+
+
+if __name__ == "__main__":
+    assert find_circle_num([[1, 1, 0], [1, 1, 0], [0, 0, 1]]) == 2
+    assert find_circle_num([[1, 0, 0], [0, 1, 0], [0, 0, 1]]) == 3
