6a5c1fe99d37c90800353cdd7aa20983483280f4
iamgodot <xugodot@gmail.com>
2025-04-26 12:06:02 -0700

    Update hash map/set problems

--- data_structures/hashtable/longest_consecutive_sequence.py
+++ data_structures/hashtable/longest_consecutive_sequence.py
@@ -1,23 +1,20 @@
-# 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
-# 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+# Longest Consecutive Sequence
+# https://leetcode.com/problems/longest-consecutive-sequence/
 
 
-from typing import List
-
-
-def longest_consecutive(nums: List[int]) -> int:
+def longest_consecutive(nums: list[int]) -> int:
     """
-    对于可能的连续序列结果，只有当 first 元素减一的元素不存在才开始计算。
     Time: O(n)
     Space: O(n)
     """
-    s = set(nums)
+    # NOTE: initialize set with all numbers
+    hash_set = set(nums)
     res = 0
-    for num in nums:
-        if num - 1 not in s:
+    for num in hash_set:  # NOTE: use hash_set to avoid duplicate elements
+        if num - 1 not in hash_set:
             count = 1
             start = num
-            while start + 1 in s:
+            while start + 1 in hash_set:
                 count += 1
                 start += 1
             res = max(res, count)
--- data_structures/hashtable/two_sum.py
+++ data_structures/hashtable/two_sum.py
@@ -1,18 +1,9 @@
-# 给定数字数组和一个目标整数，在数组中找到和为目标值的两个数字，并返回下标。
-# 重点是答案里不能是同一个元素，所以要考虑目标值是某个数字两倍的情况。
-
-
-from typing import List
+# Two Sum
+# https://leetcode.com/problems/two-sum/
 
 
 def two_sum(nums: list, target: int) -> list:
     """
-    使用哈希表将嵌套遍历优化为单次遍历：
-
-    可以先遍历数组构造哈希表，然后再次遍历查找。在第一次遍历的时候对重复数字直接替换，这样也可以避免答案中出现同一个元素。
-
-    更好的做法是只遍历一次，过程中先检查哈希表中有没有可用的答案，如果没有再将该元素更新到哈希表中，从而解决了重复元素的问题。
-
     Time: O(n)
     Space: O(n)
     """
@@ -28,13 +19,11 @@ def two_sum(nums: list, target: int) -> list:
     return []
 
 
-# 给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
-# 以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
-# 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
-# 你所设计的解决方案必须只使用常量级的额外空间。
+# Two Sum II - Input Array Is Sorted
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 
 
-def two_sum2(numbers: List[int], target: int) -> List[int]:
+def two_sum2(numbers: list[int], target: int) -> list[int]:
     i, j = 0, len(numbers) - 1
     while i < j:
         total = numbers[i] + numbers[j]
